37ac450b4dead88f67d9d6b0377580eaeb19dd90
Sunny Kumar <goodluck14115@gmail.com>
2024-04-27 23:14:55 +0530

    Update side-nav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -5,29 +5,6 @@ import { IoHomeSharp, IoSearch } from 'react-icons/io5'
 import { MdAddBox } from 'react-icons/md'
 import Logo from './Logo'
 
-const NAVIGATION_ITEMS = [
-  {
-    title: 'Home',
-    icon: IoHomeSharp
-  },
-  {
-    title: 'Search',
-    icon: IoSearch
-  },
-  {
-    title: 'Create',
-    icon: MdAddBox
-  },
-  {
-    title: 'Notification',
-    icon: IoMdNotifications
-  },
-  {
-    title: 'Profile',
-    icon: FaUser
-  }
-]
-
 const SideNav = () => {
   return (
     <section className="flex md:flex-col w-screen md:w-60 bg-white md:border-r-[1px] border-t-[1px] md:border-t-0 border-gray-200 h-12 md:h-screen fixed md:p-3 bottom-0">
@@ -36,22 +13,42 @@ const SideNav = () => {
       </div>
       <div className="flex md:flex-col w-full h-full justify-between">
         <div className="flex md:flex-col w-full justify-between">
-          {NAVIGATION_ITEMS.map((item) => (
-            <Link
-              className="flex h-full w-full items-center justify-center md:justify-start hover:bg-gray-200 rounded-lg px-4 py-3 mb-2 text-[17px]  space-x-4"
-              href={
-                item.title === 'Home'
-                  ? '/'
-                  : `/${item.title.toLocaleLowerCase()}`
-              }
-              key={item.title}
-            >
-              <div className="text-2xl">
-                <item.icon />
-              </div>
-              <div className="hidden md:block">{item.title}</div>
-            </Link>
-          ))}
+          <Link
+            className="flex h-full w-full items-center justify-center md:justify-start hover:bg-gray-200 rounded-lg px-4 py-3 mb-2 text-[17px]  space-x-4"
+            href="/"
+          >
+            <div className="text-2xl">
+              <IoHomeSharp />
+            </div>
+            <div className="hidden md:block">Home</div>
+          </Link>
+          <div className="flex h-full w-full items-center justify-center cursor-pointer md:justify-start hover:bg-gray-200 rounded-lg px-4 py-3 mb-2 text-[17px]  space-x-4">
+            <div className="text-2xl">
+              <IoSearch />
+            </div>
+            <div className="hidden md:block">Search</div>
+          </div>
+          <div className="flex h-full w-full items-center justify-center cursor-pointer md:justify-start hover:bg-gray-200 rounded-lg px-4 py-3 mb-2 text-[17px]  space-x-4">
+            <div className="text-2xl">
+              <MdAddBox />
+            </div>
+            <div className="hidden md:block">Create</div>
+          </div>
+          <div className="flex h-full w-full items-center justify-center cursor-pointer md:justify-start hover:bg-gray-200 rounded-lg px-4 py-3 mb-2 text-[17px]  space-x-4">
+            <div className="text-2xl">
+              <IoMdNotifications />
+            </div>
+            <div className="hidden md:block">Notification</div>
+          </div>
+          <Link
+            className="flex h-full w-full items-center justify-center md:justify-start hover:bg-gray-200 rounded-lg px-4 py-3 mb-2 text-[17px]  space-x-4"
+            href="/profile"
+          >
+            <div className="text-2xl">
+              <FaUser />
+            </div>
+            <div className="hidden md:block">Profile</div>
+          </Link>
         </div>
         <div className="hover:bg-gray-300 cursor-pointer rounded-xl px-4 py-3 mb-2 text-[17px] hidden md:flex items-center justify-start space-x-4">
           <div className="text-2xl">
