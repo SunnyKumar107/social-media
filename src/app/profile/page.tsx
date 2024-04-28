b88f7995fe29318750423f3a38ba84447c68f787
Sunny Kumar <goodluck14115@gmail.com>
2024-04-28 20:58:39 +0530

    Change style in profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,9 +4,9 @@ const page = () => {
   return (
     <div className="flex flex-col justify-start mt-14 mb-12 md:mt-0 md:ml-60 md:px-4 lg:px-16 py-4 md:py-8">
       <div className="px-0 md:px-12 border-b w-full h-fit border-gray-300">
-        <div className="flex w-screen md:w-full px-2">
-          <div className="w-[70px] h-[70px] md:w-36 md:h-36 rounded-full bg-gray-200 mr-8 md:mr-16"></div>
-          <div className="flex justify-between w-full max-w-[400px] text-center px-4 py-2">
+        <div className="flex gap-4 md:gap-8 w-screen md:w-full px-2">
+          <div className="min-w-[70px] min-h-[70px] md:w-28 md:h-28 lg:w-36 lg:h-36 rounded-full bg-gray-200"></div>
+          <div className="flex justify-between w-full max-w-[350px] text-center px-4 py-2 md:py-8">
             <div>
               <h4 className="font-medium text-lg">4</h4>
               <p className="text-gray-600 text-sm">post</p>
@@ -21,11 +21,11 @@ const page = () => {
             </div>
           </div>
         </div>
-        <div className="px-2 py-4">
+        <div className="p-2">
           <h4 className="font-medium text-lg">John Smith</h4>
           <p className="text-gray-800">bio toh abhi ban hi raha hai.</p>
         </div>
-        <div className="flex w-full mt-4 gap-4 px-2">
+        <div className="flex w-full gap-4 p-2">
           <button className="bg-gray-200 max-w-72 hover:bg-gray-300 text-sm font-medium rounded-md w-full py-1  space-x-4">
             Edit Profile
           </button>
@@ -37,9 +37,9 @@ const page = () => {
       <div className="">
         <h2 className="text-sm font-medium p-2">POSTS</h2>
         <div className="flex flex-wrap w-full justify-between">
-          <div className="w-[120px] md:w-52 lg:w-80 h-[120px] md:h-52 lg:h-80 bg-gray-200"></div>
-          <div className="w-[120px] md:w-52 lg:w-80 h-[120px] md:h-52 lg:h-80 bg-gray-200"></div>
-          <div className="w-[120px] md:w-52 lg:w-80 h-[120px] md:h-52 lg:h-80 bg-gray-200"></div>
+          <div className="w-[123px] md:w-52 lg:w-80 h-[123px] md:h-52 lg:h-80 bg-gray-200"></div>
+          <div className="w-[123px] md:w-52 lg:w-80 h-[123px] md:h-52 lg:h-80 bg-gray-200"></div>
+          <div className="w-[123px] md:w-52 lg:w-80 h-[123px] md:h-52 lg:h-80 bg-gray-200"></div>
         </div>
       </div>
     </div>
